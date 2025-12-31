96c5fe1fd036e50f4de63249669aa510a689c509
kukushkart <kukulianskiy@gmail.com>
2025-12-31 09:01:52 -0600

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Number.h"
+
+int main() {
+    Number a(10);
+    Number b(2);
+    Number c = a / b;
+
+    std::cout << "Result: " << c.GetValue() << std::endl;
+    return 0;
+}
